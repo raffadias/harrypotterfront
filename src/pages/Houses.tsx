78e926a5f9474481e73e61935b8a147700dc8f6e
Rafael Ribeiro Dias <rafael.dias@evolutionsoft.com.br>
2022-08-24 22:21:35 -0300

    all houses pages responsive

--- src/pages/Houses.tsx
+++ src/pages/Houses.tsx
@@ -26,7 +26,7 @@ export default function Houses() {
     }, []);
     return (
         <>
-            <div className="bg-gray-700 h-screen w-auto">
+            <div className="bg-gray-700 h-screen cel:h-full w-auto">
                 <div className="flex flex-col text-center font-aboreto">
                     <Sidebar />
                     
@@ -35,53 +35,53 @@ export default function Houses() {
                             <Spinner/>
                         </div>
                     )}
-                    <div className="flex ml-14">
+                    <div className="flex ml-14 lg:flex-wrap">
                         {houses?.map((house: any) => (
                             <>
                                 <Link
                                     to={`/${house.name.toLowerCase()}`}
                                     className={`
-                                h-[39rem] w-[18rem] bg-gray-900 mt-[3rem] mx-auto rounded-xl shadow-lg group
+                                h-auto w-[18rem] xl:w-[13rem] bg-gray-900 mt-[3rem] lg:mt-[1rem] mx-auto rounded-xl shadow-lg group
                                 ${house.name === 'Gryffindor' && 'hover:bg-[#690120]'} 
                                 ${house.name === 'Slytherin' && 'hover:bg-[#1a472a]'} 
                                 ${house.name === 'Hufflepuff' && 'hover:bg-[#b48c1d]'} 
                                 ${house.name === 'Ravenclaw' && 'hover:bg-[#0E1A40]'}
-                                hover:transition-all hover:duration-200 hover:ease-in-out hover:h-[40rem] hover:w-[19rem] hover:mt-[2rem]`}
+                                hover:transition-all hover:duration-200 hover:ease-in-out hover:mt-[2rem]`}
                                 >
-                                    {house.name === 'Gryffindor' && (<img src={Gryffindor} alt="" className="h-[12rem] w-[10rem] mx-auto mt-5" />)}
-                                    {house.name === 'Slytherin' && (<img src={Slytherin} alt="" className="h-[12rem] w-[11rem] mx-auto mt-5" />)}
-                                    {house.name === 'Hufflepuff' && (<img src={Hufflepuff} alt="" className="h-[12rem] w-[10rem] mx-auto mt-5" />)}
-                                    {house.name === 'Ravenclaw' && (<img src={Ravenclaw} alt="" className="h-[12rem] w-[10rem] mx-auto mt-5" />)}
-                                    <h2 className={`text-white font-bold mt-5 mb-10`}>{house.name}</h2>
+                                    {house.name === 'Gryffindor' && (<img src={Gryffindor} alt="" className="logo-size" />)}
+                                    {house.name === 'Slytherin' && (<img src={Slytherin} alt="" className="logo-size" />)}
+                                    {house.name === 'Hufflepuff' && (<img src={Hufflepuff} alt="" className="logo-size" />)}
+                                    {house.name === 'Ravenclaw' && (<img src={Ravenclaw} alt="" className="logo-size" />)}
+                                    <h2 className={`text-white font-bold mt-5 lg:mt-2 lg:mb-4 mb-10`}>{house.name}</h2>
                                     <p className="text-white font-bold text-[11px]">Founder: {house.founder}</p>
                                     <div className="flex justify-center align-center mt-5">
                                         <p className="text-white font-bold text-[11px] self-center">Colors:</p>
                                         {house.name === 'Gryffindor' && (
                                             <>
-                                                <div className="h-8 w-8 bg-[#990230] ml-5 rounded-full shadow-sm"></div>
-                                                <div className="h-8 w-8 bg-amber-300 ml-5 rounded-full shadow-sm"></div>
+                                                <div className="houses-color bg-[#990230] "></div>
+                                                <div className="houses-color bg-amber-300"></div>
                                             </>
                                         )}
                                         {house.name === 'Slytherin' && (
                                             <>
-                                                <div className="h-8 w-8 bg-green-900 ml-5 rounded-full shadow-sm"></div>
-                                                <div className="h-8 w-8 bg-white ml-5 rounded-full shadow-sm"></div>
+                                                <div className="houses-color bg-green-900"></div>
+                                                <div className="houses-color bg-white"></div>
                                             </>
                                         )}
                                         {house.name === 'Hufflepuff' && (
                                             <>
-                                                <div className="h-8 w-8 bg-amber-300 ml-5 rounded-full shadow-sm"></div>
-                                                <div className="h-8 w-8 bg-gray-600 ml-5 rounded-full shadow-sm"></div>
+                                                <div className="houses-color bg-amber-300"></div>
+                                                <div className="houses-color bg-gray-600"></div>
                                             </>
                                         )}
                                         {house.name === 'Ravenclaw' && (
                                             <>
-                                                <div className="h-8 w-8 bg-blue-900 ml-5 rounded-full shadow-sm"></div>
-                                                <div className="h-8 w-8 bg-[#B08D57] ml-5 rounded-full shadow-sm"></div>
+                                                <div className="houses-color bg-blue-900"></div>
+                                                <div className="houses-color bg-[#B08D57]"></div>
                                             </>
                                         )}
                                     </div>
-                                    <label className="text-white font-bold flex justify-center align-center mt-5">
+                                    <label className="text-white font-bold flex justify-center align-center mt-5 lg:mt-2">
                                         Traits <AiOutlineArrowDown size="12" className="text-white self-center ml-3" />
                                     </label>
                                     <ul className="list-none flex flex-col">
@@ -91,7 +91,7 @@ export default function Houses() {
                                             </li>
                                         ))}
                                     </ul>
-                                    <label className="text-white font-bold flex justify-center align-center mt-5">
+                                    <label className="text-white font-bold flex justify-center align-center mt-5 mb-5 lg:hidden">
                                         Element: {house.element}
                                     </label>
                                 </Link>
