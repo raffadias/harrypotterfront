e8ba6fe713c9f5da86d9e6c7e2f2af1c5701aa04
Rafael Ribeiro Dias <rafael.dias@evolutionsoft.com.br>
2022-08-24 22:56:19 -0300

    fix: home responsive

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Maincontent from "./components/Maincontent";
 function App() {
 
   return (
-    <div className="flex bg-gray-700 h-screen">
+    <div className="flex bg-gray-700 h-screen lg:h-full">
       <Sidebar/>
       <Maincontent/>
     </div>
